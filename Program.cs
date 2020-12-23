9558464b1a68f8146ec86e8211d2ef6279a219a4
kimilao01 <76526337+kimilao01@users.noreply.github.com>
2020-12-24 07:42:13 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace prime_number_checker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Enter a number: ");
+                long num = Convert.ToInt64(Console.ReadLine());
+                int factors = 0;
+                for (int i = 1; i <= num; i++)
+                {
+                    if (num % i == 0)
+                    {
+                        factors++;
+                    }
+                }
+                if (factors == 2)
+                {
+                    Console.WriteLine("Yes, it is a prime number.");
+                }
+                else
+                {
+                    Console.WriteLine("No, it is not a prime number.");
+                }
+            }
+        }
+    }
+}
